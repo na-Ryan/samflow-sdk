dad3e6c7a139f54d681cc2bb6db8fd1e179228d0
Narayanan Chandran <naryan12ster@gmail.com>
2022-09-21 18:51:05 +0530

    added status

---
+++ src/core/STATUS.ts
@@ -0,0 +1,6 @@
+export let STATUS  = {
+    STARTED : "1",
+    IN_PROGRESS : "2",
+    ERROR : "3",
+    COMPLETED : "4"
+}
--- src/core/TaskModel.ts
+++ src/core/TaskModel.ts
@@ -1,17 +1,25 @@
+import { STATUS } from "./STATUS";
+
 export abstract class TaskModel {
     createdAt : number;
     completedAt : number;
     status : string;
-    instance_id : string;
-    key : string;
+    instanceId : string;
+    taskId : string;
     inputData : object;
     outputData : object;
     taskName : string;
-    version : number;
-
-
-    constructor(){
+    version : string;
+    
+    constructor(options : { instanceId: string, inputData : any, taskName : string, version : string }){
         this.createdAt = Date.now();
+        this.completedAt = null;
+        this.status = STATUS.STARTED;
+        this.instanceId = options.instanceId;
+        this.taskId = "1"; // create new id we can use some lib here 
+        this.inputData = options.inputData;
+        this.taskName = options.taskName;
+        this.version = options.version;
 
     }
 }
