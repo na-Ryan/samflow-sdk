fa3a04426834388c4a5f321eb1b676f8a7363e5c
Narayanan Chandran <naryan12ster@gmail.com>
2022-09-21 17:01:35 +0530

    add more variables

--- src/core/TaskModel.ts
+++ src/core/TaskModel.ts
@@ -1,8 +1,17 @@
 export abstract class TaskModel {
     createdAt : number;
+    completedAt : number;
     status : string;
-    
+    instance_id : string;
+    key : string;
+    inputData : object;
+    outputData : object;
+    taskName : string;
+    version : number;
+
+
     constructor(){
         this.createdAt = Date.now();
+
     }
 }
